0c56fc8744f235840abb70bc6daf2fcc8d8765a0
Kepler9870u0987 <m.albanese.manuel@gmail.com>
2026-02-12 23:14:12 +0100

    Initialize PII redaction module with core functionality and exports

--- src/eml_classificator/pii/__init__.py
+++ src/eml_classificator/pii/__init__.py
@@ -1,1 +1,13 @@
 # PII redaction module
+
+from .redactor import PII_REDACTION_VERSION, PIIRedactor, RedactionLevel
+from .hash_utils import create_salt, hash_with_salt, verify_hash
+
+__all__ = [
+    "PIIRedactor",
+    "RedactionLevel",
+    "PII_REDACTION_VERSION",
+    "hash_with_salt",
+    "create_salt",
+    "verify_hash",
+]
